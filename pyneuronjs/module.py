369f799ac476ef5ca2889fe4893b0b84e8d107c1
Kael <i@kael.me>
2015-11-15 18:21:19 +0800

    finishes module.py

--- pyneuronjs/module.py
+++ pyneuronjs/module.py
@@ -6,38 +6,32 @@ import re
 def module_id(name, version, path=''):
     # 'a', '*', '' -> 'a@*/a.js'
     # 'a', '*', '/' -> 'a@*/a.js'
-    if not path or path == '/':
-        path = '/' + name + '.js'
+    if not path or path == b'/':
+        path = b'/' + name + b'.js'
 
     return package_id(name, version) + path
 
 
 def package_id(name, version):
-    return name + '@' + version
+    return name + b'@' + version
 
 
 REGEX_MODULE_ID = re.compile(
-    r"""^(
-            [^\/]+?     # name
-        )
+    r"""^
+        ([^\/]+?)       # name
         (?:
-            @(
-                [^\/]+  # version
-            )
-        )?              # optinal version
-        (
-            \/.*        # path
-        )?$""",
-
+            @
+            ([^\/]+)    # version
+        )?
+        (\/.*)?         # path
+        $""",
     re.X)
 
 def parse_module_id(id):
     # there will always a match
     m = re.match(REGEX_MODULE_ID, id)
-
-# def parse_package_id(id):
-#     splitted = id.split('@')
-#     if len(splitted) == 1:
-#         return (package_id, '*')
-
-#     return (splitted[0], splitted[1])
+    return (
+        m.group(1),
+        # version default to '*'
+        m.group(2) or b'*' ,
+        m.group(3) or b''  )
