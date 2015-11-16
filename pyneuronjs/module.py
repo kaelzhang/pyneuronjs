dfc54edbe81511248d69236dd3a08b132078ac52
Kael <i@kael.me>
2015-11-16 11:13:38 +0800

    test: adds test cases for module.py, fixes bugs

--- pyneuronjs/module.py
+++ pyneuronjs/module.py
@@ -1,8 +1,11 @@
 # Utility tools to parse a module
 
+
 import re
 
+
 # format to module id
+# 'jquery' -> 'jquery@*/jquery.js'
 def module_id(name, version, path=''):
     # 'a', '*', '' -> 'a@*/a.js'
     # 'a', '*', '/' -> 'a@*/a.js'
@@ -27,11 +30,17 @@ REGEX_MODULE_ID = re.compile(
         $""",
     re.X)
 
+
 def parse_module_id(id):
     # there will always a match
     m = re.match(REGEX_MODULE_ID, id)
+    path = m.group(3) or b''
+
+    if path == b'/':
+        path = b''
+
     return (
         m.group(1),
         # version default to '*'
-        m.group(2) or b'*' ,
-        m.group(3) or b''  )
+        m.group(2) or b'*',
+        path)
--- test/module.py
+++ test/module.py
@@ -1,6 +1,64 @@
 #!/usr/bin/env python
 
+import unittest
+import sys
+
 from env import ABSPATH
 import pyneuronjs.module as module
 
-print module.parse_module_id('jquery').group(2)
+
+class TestModuleParser(unittest.TestCase):
+
+    def test_parse_module_id_name(self):
+        id = b'jquery'
+        (name, version, path) = module.parse_module_id(id)
+
+        self.assertEqual(name, b'jquery')
+        self.assertEqual(version, b'*')
+        self.assertEqual(path, b'')
+
+    def test_parse_module_id_name_version(self):
+        id = b'jquery@1.0.0'
+        (name, version, path) = module.parse_module_id(id)
+
+        self.assertEqual(name, b'jquery')
+        self.assertEqual(version, b'1.0.0')
+        self.assertEqual(path, b'')
+
+    def test_parse_module_id_name_version_path(self):
+        id = b'jquery@1.0.0/a.js'
+        (name, version, path) = module.parse_module_id(id)
+
+        self.assertEqual(name, b'jquery')
+        self.assertEqual(version, b'1.0.0')
+        self.assertEqual(path, b'/a.js')
+
+        id = b'jquery@1.0.0/'
+        (name, version, path) = module.parse_module_id(id)
+
+        self.assertEqual(name, b'jquery')
+        self.assertEqual(version, b'1.0.0')
+        self.assertEqual(path, b'')
+
+    def test_module_id(self):
+        id = module.module_id('jquery', '*')
+        self.assertEqual(id, 'jquery@*/jquery.js')
+
+        id = module.module_id('jquery', '*', '')
+        self.assertEqual(id, 'jquery@*/jquery.js')
+
+        id = module.module_id('jquery', '*', '/')
+        self.assertEqual(id, 'jquery@*/jquery.js')
+
+        id = module.module_id('jquery', '*', '/a.js')
+        self.assertEqual(id, 'jquery@*/a.js')
+
+        id = module.module_id('jquery', '1.1.0', '/a.js')
+        self.assertEqual(id, 'jquery@1.1.0/a.js')
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestModuleParser)
+runner = unittest.TextTestRunner(verbosity=2).run(suite)
+
+exit_code = 0 if runner.wasSuccessful() else 1
+sys.exit(exit_code)
